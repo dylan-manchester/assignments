6cc014c40bbf88381a0322d02b751f716de02cd6
dylan-manchester <78382743+dylan-manchester@users.noreply.github.com>
2021-10-28 12:42:04 -0700

    delete old files

--- CRUDFullStack/backend/src/main/java/com/hcl/spring/demo/DAO/JpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.spring.demo.DAO;
-
-import com.hcl.spring.demo.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaRepo extends JpaRepository<Employee, Long> {
-}
