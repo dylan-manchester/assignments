ca7a1ad4d1937e2d13a957feb281f9da048bbbaf
Dylan Manchester <dylan.manchester@uconn.edu>
2021-11-08 08:57:35 -0800

    Fixed Typo

--- CRUDFullStack/backend/src/main/java/com/hcl/spring/demo/models/Employee.java
+++ CRUDFullStack/backend/src/main/java/com/hcl/spring/demo/models/Employee.java
@@ -23,6 +23,10 @@ public class Employee {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
